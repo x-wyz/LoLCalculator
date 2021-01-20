84c5356960c92e4e7a4ffcc66ef213189eda9c44
Wilson Ye <nextyearwillbebetter1@gmail.com>
2021-01-20 14:06:06 -0800

    added starting and basic items to items data

---
+++ src/data/items.js
@@ -0,0 +1,169 @@
+export const itemsData = {
+	cull: {
+		img: 1083,
+		tier: 'basic',
+		stats: {
+			attack: 7
+		}
+	},
+	doransBlade: {
+		img: 1055,
+		tier: 'basic',
+		stats: {
+			attack: 8,
+			hp: 80
+		}
+	},
+	doransShield: {
+		img: 1054,
+		tier: 'basic',
+		stats: {
+			hp: 80
+		}
+	},
+	doransRing: {
+		img: 1056,
+		tier: 'basic',
+		stats: {
+			hp: 70,
+			ap: 15
+		}
+	},
+	amplifyingTomb: {
+		img: 1052,
+		tier: 'basic',
+		stats: {
+			ap: 20
+		}
+	},
+	bFSword: {
+		img: 1038,
+		tier: 'basic',
+		stats: {
+			attack: 40
+		}
+	},
+	blastingWand: {
+		img: 1026,
+		tier: 'basic',
+		stats: {
+			ap: 40
+		}
+	},
+	cloakOfAgility: {
+		img: 1018,
+		tier: 'basic',
+		stats: {
+			critChance: 15
+		}
+	},
+	clothArmor: {
+		img: 1029,
+		tier: 'basic',
+		stats: {
+			armor: 15
+		}
+	},
+	dagger: {
+		img: 1042,
+		tier: 'basic',
+		stats: {
+			attackspeed: 12
+		}
+	},
+	darkSeal: {
+		img: 1082,
+		tier: 'basic',
+		stats: {
+			hp: 40,
+			ap: 15
+		}
+	},
+	longSword: {
+		img: 1036,
+		tier: 'basic',
+		stats: {
+			attack: 10
+		}
+	},
+	needlesslyLargeRod: {
+		img: 1058,
+		tier: "basic",
+		stats: {
+			ap: 60
+		}
+	},
+	nullMagicMantle: {
+		img: 1033,
+		tier: 'basic',
+		stats: {
+			resist: 25
+		}
+	},
+	pickaxe: {
+		img: 1037,
+		tier: 'basic',
+		stats: {
+			ap: 25
+		}
+	},
+	relicShield: {
+		img: 3858,
+		tier: 'basic',
+		stats: {
+			ap: 5,
+			hp: 30
+		}
+	},
+	spellthiefsEdge: {
+		img: 3850,
+		tier: 'basic',
+		stats: {
+			hp: 10,
+			ap: 8
+		}
+	},
+	steelShoulderguards: {
+		img: 3854,
+		tier: 'basic',
+		stats: {
+			attack: 3,
+			hp: 30
+		}
+	},
+	spectralSickle: {
+		img: 3862,
+		tier: 'basic',
+		stats: {
+			attack: 5,
+			hp: 10
+		}
+	},
+	rubyCrystal: {
+		img: 1028,
+		tier: 'basic',
+		stats: {
+			hp: 150
+		}
+	},
+	sapphireCrystal: {
+		img: 1027,
+		tier: "basic",
+		stats: {
+			mana: 250
+		}
+	},
+	tearOfTheGoddess: {
+		img: 3070,
+		tier: "basic",
+		stats: {
+			mana: 240
+		},
+		effect: {
+			type: 'stats',
+			stats: {
+				mana: 360
+			}
+		}
+	}
+}
