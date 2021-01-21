08cfb96227eaf5aa32279a140a113dfef2599955
Wilson Ye <nextyearwillbebetter1@gmail.com>
2021-01-20 17:13:03 -0800

    advanced items data

--- src/data/items.js
+++ src/data/items.js
@@ -165,5 +165,387 @@ export const itemsData = {
 				mana: 360
 			}
 		}
-	}
+	},
+	aegisOfTheLegion: {
+		img: 3105,
+		tier: "advance",
+		stats: {
+			armor: 30,
+			resist: 30,
+			abilityHaste: 10
+		}
+	},
+	aetherWisp: {
+		img: 3113,
+		tier: "advance",
+		stats: {
+			ap: 30
+		}
+	},
+	bamisCinder: {
+		img: 6660,
+		tier: "advance",
+		stats: {
+			hp: 300
+		},
+		effect: {
+			type: "damage",
+			base: 12,
+			max: 12,
+			scale: "1 bHp"
+		}
+	},
+	bandleglassMirror: {
+		img: 4642,
+		tier: "advance",
+		stats: {
+			abilityHaste: 10,
+			ap: 20
+		}
+	},
+	blightingJewel: {
+		img: 4630,
+		tier: "advance",
+		stats: {
+			ap: 25,
+			mpen: 15
+		}
+	},
+	brambleVest: {
+		img: 3076,
+		tier: "advance",
+		stats: {
+			armor: 35
+		},
+		effect: {
+			type: "damage",
+			base: 3,
+			max: 3,
+			scale: "10 bAr"
+		}
+	},
+	caulfieldsWarhammer: {
+		img: 3133,
+		tier: "advance",
+		stats: {
+			attack: 25,
+			abilityHaste: 10
+		}
+	},
+	chainVest: {
+		img: 1031,
+		tier: "advance",
+		stats: {
+			armor: 40
+		}
+	},
+	crystallineBracer: {
+		img: 3801,
+		tier: "advance",
+		stats: {
+			hp: 200
+		}
+	},
+	executionersCalling: {
+		img: 3123,
+		tier: "advance",
+		stats: {
+			attack: 15
+		}
+	},
+	fiendishCodex: {
+		img: 3108,
+		tier: "advance",
+		stats: {
+			ap: 35,
+			abilityHaste: 10
+		}
+	},
+	frostFang: {
+		img: 3851,
+		tier: "advance",
+		stats: {
+			hp: 70,
+			ap: 15
+		}
+	},
+	giantsBelt: {
+		img: 1011,
+		tier: "advance",
+		stats: {
+			hp: 350
+		}
+	},
+	glacialBuckler: {
+		img: 3024,
+		tier: "advance",
+		stats: {
+			mana: 250,
+			armor: 20,
+			abilityHaste: 10
+		}
+	},
+	harrowingCrescent: {
+		img: 3862,
+		tier: "advance",
+		stats: {
+			hp: 60,
+			attack: 10
+		}
+	},
+	hearthboundAxe: {
+		img: 3051,
+		tier: "advance",
+		stats: {
+			attack: 15,
+			attackspeed: 15
+		}
+	},
+	hexdrinker: {
+		img: 3155,
+		tier: "advance",
+		stats: {
+			attack: 20,
+			resist: 35
+		}
+	},
+	hextechAlternator: {
+		img: 3145,
+		tier: "advance",
+		stats: {
+			ap: 40
+		},
+		effect: {
+			type: 'damage',
+			base: 50,
+			max: 125,
+			scale: "0 ap"
+		}
+	},
+	ironspikeWhip: {
+		img: 6029,
+		tier: "advance",
+		stats: {
+			attack: 30
+		},
+		effect: {
+			type: 'damage',
+			base: 0,
+			max: 0,
+			scale: "75 tAd"
+		}
+	},
+	kindleGem: {
+		img: 3067,
+		tier: "advance",
+		stats: {
+			hp: 200,
+			abilityHaste: 10
+		}
+	},
+	kircheisShard: {
+		img: 2015,
+		tier: "advance",
+		stats: {
+			attackspeed: 15
+		},
+		effect: {
+			type: 'damage',
+			base: 80,
+			max: 80,
+			scale: "0 ap"
+		}
+	},
+	lastWhisper: {
+		img: 3035,
+		tier: "advance",
+		stats: {
+			attack: 20,
+			arpen: 20
+		}
+	},
+	leechingLeer: {
+		img: 4635,
+		tier: "advance",
+		stats: {
+			ap: 20,
+			hp: 150
+		}
+	},
+	lostChapter: {
+		img: 3802,
+		tier: "advance",
+		stats: {
+			mana: 300,
+			ap: 40,
+			abilityHaste: 10
+		}
+	},
+	negatronCloak: {
+		img: 1057,
+		tier: "advance",
+		stats: {
+			resist: 50
+		}
+	},
+	noonquiver: {
+		img: 6670,
+		tier: "advance",
+		stats: {
+			attack: 30,
+			attackspeed: 15
+		}
+	},
+	oblivionOrb: {
+		img: 3916,
+		tier: "advance",
+		stats: {
+			ap: 30
+		}
+	},
+	phage: {
+		img: 3044,
+		tier: "advance",
+		stats: {
+			hp: 200,
+			attack: 15
+		}
+	},
+	quicksilverSash: {
+		img: 3140,
+		tier: "advance",
+		stats: {
+			resist: 30
+		}
+	},
+	rageknife: {
+		img: 6677,
+		tier: "advance",
+		stats: {
+			attackspeed: 25
+		}
+	},
+	recurveBow: {
+		img: 1043,
+		tier: "advance",
+		stats: {
+			attackspeed: 25
+		},
+		stats: {
+			type: 'onhit',
+			damage: 15,
+			max: 0,
+			current: 0
+		}
+	},
+	runesteelSpaulder: {
+		img: 3855,
+		tier: "advance",
+		stats: {
+			hp: 100,
+			attack: 6
+		}
+	},
+	seekersArmguard: {
+		img: 3191,
+		tier: "advance",
+		stats: {
+			ap: 20,
+			armor: 15
+		}
+		effect: {
+			type: 'stats',
+			stats: {
+				armor: 30
+			}
+		}
+	},
+	serratedDirk: {
+		img: 3134,
+		tier: "advance",
+		stats: {
+			attack: 30,
+			lethality: 10
+		}
+	},
+	sheen: {
+		img: 3057,
+		tier: "advance",
+		stats: {
+		},
+		effect: {
+			type: 'damage',
+			base: 0,
+			max: 0,
+			scale: "100 baseAd"
+		}
+	},
+	spectresCowl: {
+		img: 3211,
+		tier: "advance",
+		stats: {
+			hp: 250,
+			resist: 25
+		}
+	},
+	targonsBuckler: {
+		img: 3859,
+		tier: "advance",
+		stats: {
+			ap: 10,
+			hp: 100
+		}
+	},
+	tiamat: {
+		img: 3077,
+		tier: "advance",
+		stats: {
+			attack: 25
+		}
+	},
+	vampiricScepter: {
+		img: 1053,
+		tier: "advance",
+		stats: {
+			attack: 15,
+			lifesteal: 10
+		}
+	},
+	verdantBarrier: {
+		img: 4632,
+		tier: "advance",
+		stats: {
+			ap: 25,
+			resist: 25
+		}
+	},
+	wardensMail: {
+		img: 3082,
+		tier: "advance",
+		stats: {
+			armor: 40
+		}
+	},
+	watchfulWardstone: {
+		img: 4638,
+		tier: "advance",
+		stats: {
+			abilityHaste: 25
+		}
+	},
+	wingedMoonplate: {
+		img: 3066,
+		tier: "advance",
+		stats: {
+			hp: 150
+		}
+	},
+	zeal: {
+		img: 3086,
+		tier: "advance",
+		stats: {
+			attackspeed: 18,
+			critChance: 15
+		}
+	},
 }
