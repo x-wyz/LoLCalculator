0b8aff735e7ad39865ca0c2965f19332d9e783cb
Wilson Ye <nextyearwillbebetter1@gmail.com>
2021-02-26 17:28:48 -0800

    inspiration runes alt text

--- src/components/runeinspiration/runeinspiration.js
+++ src/components/runeinspiration/runeinspiration.js
@@ -20,57 +20,57 @@ const RuneInspiration = ({ currentrunes, updateRuneStatus }) => {
 			<h2 className="rune-tree-header"> Inspiration </h2>
 			<div className="rune-keystones-row">
 				<div className={`keystone ${currentrunes.inspiration.key1 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'key1')}>
-					<img className="keystone-image" src={Key1} />
+					<img className="keystone-image" src={Key1} alt="Inspiration keystone glacial augment"/>
 					<p className="keystone-name">Glacial Augment</p>
 				</div>
 				<div className={`keystone ${currentrunes.inspiration.key2 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'key2')}>
-					<img className="keystone-image" src={Key2} />
+					<img className="keystone-image" src={Key2} alt="Inspiration keystone spellbook"/>
 					<p className="keystone-name">Spellbook</p>
 				</div>
 				<div className={`keystone ${currentrunes.inspiration.key3 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'key3')}>
-					<img className="keystone-image" src={Key3} />
+					<img className="keystone-image" src={Key3} alt="Inspiration keystone omnistone"/>
 					<p className="keystone-name">Omnistone</p>
 				</div>
 			</div>
 			<div className="minor-runes-row">
 				<div className={`minor ${currentrunes.inspiration.node1 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'node1')}>
-					<img className="minor-image" src={Minor1} />
+					<img className="minor-image" src={Minor1} alt="Inspiration node hextech flashtraption"/>
 					<p className="minor-name">Flashtraption</p>
 				</div>
 				<div className={`minor ${currentrunes.inspiration.node2 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'node2')}>
-					<img className="minor-image" src={Minor2} />
+					<img className="minor-image" src={Minor2} alt="Inspiration node magical footwear"/>
 					<p className="minor-name">Magical Footwear</p>
 				</div>
 				<div className={`minor ${currentrunes.inspiration.node3 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'node3')}>
-					<img className="minor-image" src={Minor3} />
+					<img className="minor-image" src={Minor3} alt="Inspiration node perfect timing"/>
 					<p className="minor-name">Perfect Timing</p>
 				</div>
 			</div>
 			<div className="minor-runes-row">
 				<div className={`minor ${currentrunes.inspiration.node4 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'node4')}>
-					<img className="minor-image" src={Minor4} />
+					<img className="minor-image" src={Minor4} alt="Inspiration node future's market"/>
 					<p className="minor-name">Future's Market</p>
 				</div>
 				<div className={`minor ${currentrunes.inspiration.node5 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'node5')}>
-					<img className="minor-image" src={Minor5} />
+					<img className="minor-image" src={Minor5} alt="Inspiration node minion dematerializer"/>
 					<p className="minor-name">Dematerializer</p>
 				</div>
 				<div className={`minor ${currentrunes.inspiration.node6 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'node6')}>
-					<img className="minor-image" src={Minor6} />
+					<img className="minor-image" src={Minor6} alt="Inspiration node biscuit delivery"/>
 					<p className="minor-name">Biscuit Delivery</p>
 				</div>
 			</div>
 			<div className="minor-runes-row">
 				<div className={`minor ${currentrunes.inspiration.node7 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'node7')}>
-					<img className="minor-image" src={Minor7} />
+					<img className="minor-image" src={Minor7} alt="Inspiration node cosmic insight"/>
 					<p className="minor-name">Cosmic Insight</p>
 				</div>
 				<div className={`minor ${currentrunes.inspiration.node8 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'node8')}>
-					<img className="minor-image" src={Minor8} />
+					<img className="minor-image" src={Minor8} alt="Inspiration node approach velocity"/>
 					<p className="minor-name">Approach Velocity</p>
 				</div>
 				<div className={`minor ${currentrunes.inspiration.node9 ? "rune-active" : "rune-inactive"}`} onClick={() => updateRuneStatus('inspiration', 'node9')}>
-					<img className="minor-image" src={Minor9} />
+					<img className="minor-image" src={Minor9} alt="Inspiration node time warp tonic"/>
 					<p className="minor-name">Time Warp Tonic</p>
 				</div>
 			</div>
